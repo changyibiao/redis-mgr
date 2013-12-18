c38842f13efd3941762acf958bc5a2905e783d34
idning <idning@gmail.com>
2013-12-18 10:14:17 +0800

    we can setup sentinel now

--- bin/deploy.py
+++ bin/deploy.py
@@ -25,32 +25,39 @@ sys.path.append(os.path.join(WORKDIR, 'conf/'))
 import conf
 
 class Base:
+    '''
+    子类需要实现 _alive, deploy, status, 并初始化args包含如下变量
+    '''
     def __init__(self):
-        self.args = {}
+        self.args = {
+            'startcmd' :'',
+            'pidfile'  :'',
+            'logfile'  :'',
+        }
 
-    def alive(self):
-        logging.error("alive: not implement")
-
-    def copyfile(self):
-        logging.error("scp: not implement")
+    def deploy(self):
+        logging.error("deploy: not implement")
 
     def start(self):
         self._remote_run(self.args['startcmd'])
 
     def stop(self):
-        logging.error("stop : not implement")
+        cmd = T('cat $pidfile | xargs kill').s(self.args)
+        self._remote_run(cmd)
 
     def kill(self):
-        logging.error("kill: not implement")
+        cmd = T('cat $pidfile | xargs kill -9').s(self.args)
+        self._remote_run(cmd)
 
-    def ps(self):
-        logging.error("ps: not implement")
+    def status(self):
+        logging.error("status: not implement")
 
     def log(self):
-        logging.error("log: not implement")
+        cmd = T('tail $logfile').s(self.args)
+        print self._remote_run(cmd)
 
-    def clean(self):
-        logging.error("clean: not implement")
+    def _alive(self):
+        logging.error("_alive: not implement")
 
     def _run(self, raw_cmd):
         return common.system(raw_cmd, logging.debug)
@@ -81,12 +88,24 @@ class RedisServer(Base):
         self.args.update(conf.binarys)
         self.args['startcmd'] = T('bin/redis-server conf/redis-$port.conf').s(self.args)
 
-    def gen_conf(self):
-        content = file('conf/redis.conf').read()
         self.args['conf'] = T('$path/conf/redis-$port.conf').s(self.args)
         self.args['pidfile'] = T('$path/log/redis-$port.pid').s(self.args)
         self.args['logfile'] = T('$path/log/redis-$port.log').s(self.args)
         self.args['dir']     = T('$path/data').s(self.args)
+
+
+    def __str__(self):
+        return T('[RedisServer:$host:$port]').s(self.args)
+
+    def _alive(self):
+        cmd = T('$redis_cli -h $host -p $port info').s(self.args)
+        ret = self._run(cmd)
+        if ret.find('redis_version:') > -1:
+            return True
+        return False
+
+    def _gen_conf(self):
+        content = file('conf/redis.conf').read()
         content = T(content).s(self.args)
 
         self.args['local_config'] = T('conf/redis-$port.conf').s(self.args)
@@ -99,48 +118,115 @@ class RedisServer(Base):
 
     def deploy(self):
         self._init_dir()
+
         cmd = T('rsync -avP $redis_server $user@$host:$path/bin/ 1>/dev/null 2>/dev/null').s(self.args)
         self._run(cmd)
-        self.gen_conf()
 
-    def start(self):
-        self.deploy()
-        Base.start(self)
+        self._gen_conf()
 
-    def alive(self):
+    def status(self):
         cmd = T('$redis_cli -h $host -p $port info').s(self.args)
-        ret = self._run(cmd)
-        if ret.find('redis_version:') > -1:
-            return True
-        return False
+        print self._run(cmd)
 
+class Sentinel(RedisServer):
+    def __init__(self, user, host_port, path, masters):
+        self.masters = masters
+        self.args = {
+                'user':  user,
+                'host':  host_port.split(':')[0],
+                'port':  int(host_port.split(':')[1]),
+                'path':  path,
+                }
+        self.args.update(conf.binarys)
+        self.args['startcmd'] = T('bin/redis-sentinel conf/sentinel-$port.conf').s(self.args)
+
+        self.args['conf'] = T('$path/conf/sentinel-$port.conf').s(self.args)
+        self.args['pidfile'] = T('$path/log/sentinel-$port.pid').s(self.args)
+        self.args['logfile'] = T('$path/log/sentinel-$port.log').s(self.args)
+
+    def __str__(self):
+        return T('[Sentinel:$host:$port]').s(self.args)
+
+    def _gen_conf_section(self):
+        template = '''\
+sentinel monitor redis-$port $host $port 2
+sentinel down-after-milliseconds  redis-$port 60000
+sentinel failover-timeout redis-$port 180000
+#sentinel can-failover redis-$port yes TODO: the new version has no this cfg
+sentinel parallel-syncs redis-$port 1
+        '''
+        cfg = '\n'.join([T(template).s(master.args) for master in self.masters])
+        return cfg
+
+    def _gen_conf(self):
+        content = file('conf/sentinel.conf').read()
+        content = T(content).s(self.args)
+
+        self.args['local_config'] = T('conf/sentinel-$port.conf').s(self.args)
+        fout = open(self.args['local_config'], 'w+')
+        fout.write(content)
+        fout.write(self._gen_conf_section())
+        fout.close()
+
+        cmd = T('rsync -avP $local_config $user@$host:$path/conf/ 1>/dev/null 2>/dev/null').s(self.args)
+        self._run(cmd)
+
+    def deploy(self):
+        self._init_dir()
+
+        cmd = T('rsync -avP $redis_sentinel $user@$host:$path/bin/ 1>/dev/null 2>/dev/null').s(self.args)
+        self._run(cmd)
+
+        self._gen_conf()
 
 class Cluster():
     def __init__(self, args):
         self.args = args
+        self.all_redis = [ RedisServer(self.args['user'], hp, path) for hp, path in self.args['redis'] ]
+        masters = self.all_redis[::2]
+        self.all_sentinel = [Sentinel(self.args['user'], hp, path, masters) for hp, path in self.args['sentinel'] ]
 
     def deploy(self):
-        for redis in self.args['redis']:
-            r = RedisServer(self.args['user'], *redis)
-            if not r.alive():
-                r.start()
+        for r in self.all_redis:
+            if not r._alive():
+                r.deploy()
+            else:
+                logging.info('%s is alive' % r)
 
-
+        for s in self.all_sentinel:
+            if not s._alive():
+                s.deploy()
+            else:
+                logging.info('%s is alive' % r)
 
     def start(self):
-        for redis in self.args['redis']:
-            r = RedisServer(self.args['user'], *redis)
-            if not r.alive():
+        for r in self.all_redis:
+            if not r._alive():
                 r.start()
+            else:
+                logging.info('%s is alive' % r)
+
+        for s in self.all_sentinel:
+            if not s._alive():
+                s.start()
+            else:
+                logging.info('%s is alive' % r)
 
     def stop(self):
-        pass
+        for r in self.all_redis:
+            r.stop()
+        for r in self.all_sentinel:
+            r.stop()
 
-    def ps(self):
-        pass
+    def status(self):
+        for r in self.all_redis:
+            r.status()
+        for r in self.all_sentinel:
+            r.stop()
 
     def log(self):
-        pass
+        for r in self.all_redis:
+            r.log()
 
 def discover_op():
     import inspect
--- conf/conf.py
+++ conf/conf.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 #端口: 角色1位, 集群号1位, 实例号2位, 主从端口一样
-#proxy       9xxx
+#proxy      22xxx
 #redis      20xxx
 #sentinel   21xxx
 
@@ -23,6 +23,7 @@ cluster0 = {
     'user': 'ning',
     'redis': [
         # host:port, install path
+        ('127.0.0.5:20000', '/tmp/redis-20000'), ('127.0.0.5:30001', '/tmp/redis-30000'), #(示例配置, 主从端口分别用2xxxx/3xxxx)
         ('127.0.0.5:20001', '/tmp/redis-20001'), ('127.0.0.5:30001', '/tmp/redis-30001'), #(示例配置, 主从端口分别用2xxxx/3xxxx)
         #('127.0.0.5:20002', '/tmp/redis-20002'), ('127.0.0.5:30002', '/tmp/redis-30002'),
         #('127.0.0.5:20003', '/tmp/redis-20003'), ('127.0.0.5:30003', '/tmp/redis-30003'),
@@ -30,9 +31,9 @@ cluster0 = {
     ],
     'twemproxy': [
         # host:port, install path
-        ('127.0.0.5:9000', '/tmp/twemproxy-9000'),
-        ('127.0.0.5:9001', '/tmp/twemproxy-9001'),
-        ('127.0.0.5:9002', '/tmp/twemproxy-9002'),
+        ('127.0.0.5:22000', '/tmp/twemproxy-22000'),
+        ('127.0.0.5:22001', '/tmp/twemproxy-22001'),
+        ('127.0.0.5:22002', '/tmp/twemproxy-22002'),
         #...
     ],
     'sentinel':[
