eb6161323e6cfe0a5b152695bbf784e6ff451f2a
idning <idning@gmail.com>
2014-01-02 18:46:20 +0800

    small change
    use -n 100000 for benchmark (smaller)
    
    [ticket: X]

--- conf/conf.py
+++ conf/conf.py
@@ -1,11 +1,11 @@
 #coding: utf-8
 #the port: role: x, cluster_id: x, instance:xx
-#       1        0              x           xx
+#       2        0              x           xx
 
-#redis-master   10xxx
-#redis-slave    11xxx
-#proxy          12xxx 13xxx(status-port)
-#sentinel       19xxx
+#redis-master   20xxx
+#redis-slave    21xxx
+#proxy          22xxx 23xxx(status-port)
+#sentinel       29xxx
 
 #we will generate:
 #port
